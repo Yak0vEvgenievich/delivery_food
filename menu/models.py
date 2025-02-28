88e9793f720a52aaf552eeed7f433fb920248c27
Яков Евгеньевич <yak0v.evgenievich.bsnss@gmail.com>
2025-03-01 00:00:43 +0700

    Создание новый моделей и подкючение их к админ панели

--- menu/models.py
+++ menu/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
+
 # Create your models here.
+class Categories(models.Model):
+    name = models.CharField(max_length=150, unique=True, verbose_name='Название')
+    slug = models.SlugField(max_length=200, unique=True, blank=True, null=True, verbose_name='URL')
+
+    class Meta:
+        db_table = 'category'
+        verbose_name = 'Категорию'
+        verbose_name_plural = 'Категории'
+
+
+class Products(models.Model):
+    name = models.CharField(max_length=150, unique=True, verbose_name='Название')
+    slug = models.SlugField(max_length=200, unique=True, blank=True, null=True, verbose_name='URL')
+    description = models.TextField(blank=True, null=True, verbose_name='Описание')
+    image = models.ImageField(upload_to='menu_images', blank=True, null=True)
+    price = models.PositiveIntegerField(default=0, verbose_name='Цена')
+    discount = models.PositiveIntegerField(default=0, verbose_name='Скидка')
+    quamtity = models.PositiveIntegerField(default=0, verbose_name='Количество')
+    category = models.ForeignKey(to=Categories, on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'product'
+        verbose_name = 'Продукт'
+        verbose_name_plural = 'Продукты'
--- menu/views.py
+++ menu/views.py
@@ -4,11 +4,72 @@ from django.shortcuts import render
 # Create your views here.
 
 def catalog(request):
+    context = {
+        'title': 'Меню',
+        'goods': [
+            {'image': 'deps/images/goods/sushiset.jpeg',
+             'name': 'Сет из суши',
+             'description': 'Комплект из различных видов суши для наслаждения вкусом морепродуктов.',
+             'price': 150.00},
 
+            {'image': 'deps/images/goods/roll_salmon.jpeg',
+             'name': 'Роллы с лососем',
+             'description': 'Набор роллов с нежным лососем и авокадо, идеален для любителей рыбы.',
+             'price': 93.00},
 
+            {'image': 'deps/images/goods/roll_tunec.jpeg',
+             'name': 'Острые роллы с тунцом',
+             'description': 'Роллы с тунцом и острым соусом, для тех, кто любит пикантные блюда.',
+             'price': 670.00},
 
-    return render(request, 'menu/catalog.html')
+            {'image': 'deps/images/goods/roll_fila.jpeg',
+             'name': 'Роллы Филадельфия',
+             'description': 'Классические роллы Филадельфия с крем-сыром и огурцом.',
+             'price': 365.00},
 
+            {'image': 'deps/images/goods/roll_tempura.jpeg',
+             'name': 'Роллы Темпура',
+             'description': 'Роллы с хрустящей темпурой и овощами, отличный выбор для гурманов.',
+             'price': 430.00},
+
+            {'image': 'deps/images/goods/roll_dragon.jpeg',
+             'name': 'Дракон Ролл',
+             'description': 'Уникальные роллы с угрем и авокадо, настоящая находка для любителей экзотики!',
+             'price': 610.00},
+
+            {'image': 'deps/images/goods/roll_ovoshi.jpeg',
+             'name': 'Овощные роллы',
+             'description': 'Легкие овощные роллы, идеальны для вегетарианцев.',
+             'price': 55.00},
+
+            {'image': 'deps/images/goods/roll_crab.jpeg',
+             'name': 'Роллы с крабом',
+             'description': 'Роллы с крабом и огурцом, для истинных ценителей морепродуктов.',
+             'price': 190.00},
+
+            {'image': 'deps/images/goods/roll_spicyshrimp.jpeg',
+             'name': 'Острые роллы с креветками',
+             'description': 'Роллы с креветками и острым соусом, вкус, который не оставит вас равнодушным.',
+             'price': 30.00},
+
+            {'image': 'deps/images/goods/roll_vegan.jpeg',
+             'name': 'Веганские роллы',
+             'description': 'Веганские роллы с авокадо и огурцом, свежий и полезный выбор.',
+             'price': 10.00},
+
+            {'image': 'deps/images/goods/roll_flavor.jpeg',
+             'name': 'Цветочные роллы',
+             'description': 'Дизайнерские роллы, украшенные съедобными цветами для красивой подачи.',
+             'price': 15.00},
+
+            {'image': 'deps/images/goods/roll_ananas.jpeg',
+             'name': 'Роллы с ананасом',
+             'description': 'Роллы необычной формы, но с отличным вкусом, подойдут для любителей экспериментов.',
+             'price': 25.00},
+        ]
+    }
+
+    return render(request, 'menu/catalog.html', context)
 
 
 def product(request):
