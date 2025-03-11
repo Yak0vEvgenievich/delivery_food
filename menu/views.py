db7269dacb702b35d300a0cb7479bb5dad596225
Яков Евгеньевич <yak0v.evgenievich.bsnss@gmail.com>
2025-03-12 00:01:00 +0700

    Добавление пагинации на страницах каталога

--- menu/views.py
+++ menu/views.py
@@ -1,24 +1,27 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render, get_list_or_404
-
-
 from menu.models import Products
 
 
 # Create your views here.
 
-def catalog(request, category_slug):
+def catalog(request, category_slug, page =1):
 
     if category_slug == 'all':
         menu = Products.objects.all()
     else:
         menu = get_list_or_404(Products.objects.filter(category__slug= category_slug))
 
+    paginator = Paginator(menu, 6)
+    current_page = paginator.page(page)
+
 
 
 
     context = {
         'title': 'Меню',
-        'menu': menu,
+        'menu': current_page,
+        'slug_url': category_slug
     }
 
     return render(request, 'menu/catalog.html', context)
