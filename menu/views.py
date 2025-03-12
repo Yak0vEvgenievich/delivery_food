f72ffe5e36372be3381b629fa915406d6abf4dbc
Яков Евгеньевич <yak0v.evgenievich.bsnss@gmail.com>
2025-03-12 23:11:25 +0700

    Добавление поиска на сайте, работа с шаблонами и написание логики поиска по id,названию и описанию

--- menu/views.py
+++ menu/views.py
@@ -2,23 +2,28 @@ from django.core.paginator import Paginator
 from django.shortcuts import render, get_list_or_404
 from menu.models import Products
 
+from menu.utils import q_search
+
 
 # Create your views here.
 
-def catalog(request, category_slug):
-
+def catalog(request, category_slug=None):
     page = request.GET.get('page', 1)
+    order_by = request.GET.get('order_by', None)
+    query = request.GET.get('q', None)
 
     if category_slug == 'all':
         menu = Products.objects.all()
+    elif query:
+        menu = q_search(query)
     else:
-        menu = get_list_or_404(Products.objects.filter(category__slug= category_slug))
+        menu = get_list_or_404(Products.objects.filter(category__slug=category_slug))
+
+    if order_by and order_by != 'default':
+        menu = menu.order_by(order_by)
 
     paginator = Paginator(menu, 6)
     current_page = paginator.page(int(page))
-
-
-
 
     context = {
         'title': 'Меню',
@@ -30,11 +35,10 @@ def catalog(request, category_slug):
 
 
 def product(request, product_slug):
-
     product = Products.objects.get(slug=product_slug)
 
     context = {
         'product': product
     }
 
-    return render(request, 'menu/product.html', context= context)
+    return render(request, 'menu/product.html', context=context)
