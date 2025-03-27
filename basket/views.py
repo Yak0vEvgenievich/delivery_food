36e85e71a2dd25028857a0409a36ba483f98dea4
Яков Евгеньевич <yak0v.evgenievich.bsnss@gmail.com>
2025-03-27 22:08:39 +0700

    Если попытки в js обернутся крахом,будет куда вернутся :)

--- basket/views.py
+++ basket/views.py
@@ -5,7 +5,6 @@ from basket.models import Basket
 
 # Create your views here.
 def basket_add(request, product_slug):
-
     product = Products.objects.get(slug=product_slug)
 
     if request.user.is_authenticated:
@@ -17,14 +16,18 @@ def basket_add(request, product_slug):
                 basket.quantity += 1
                 basket.save()
         else:
-            Basket.objects.create(user=request.user, product= product, quantity = 1)
+            Basket.objects.create(user=request.user, product=product, quantity=1)
 
     # альтернатива return redirect('products:list')
     return redirect(request.META['HTTP_REFERER'])
+
 
 def basket_change(request, product_slug):
     pass
 
 
-def basket_remove(request, product_slug):
-    pass
+def basket_remove(request, basket_id):
+    basket = Basket.objects.get(id=basket_id)
+    basket.delete()
+
+    return redirect(request.META['HTTP_REFERER'])
